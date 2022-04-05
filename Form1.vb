a511f00faadb332f2df40af709b687c2f20b8cce
Administrator <Administrator@PSPKM2826-2243>
2022-04-05 11:53:15 +0800

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbMonth.SelectedIndexChanged
+        Dim strDays() As String = {"31", "28 or 29", "31", "30", "31", "31", "30", "31", "31", "30", "31", "30", "31"}
+        Dim strMonth() As String = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "Novemeber", "December"}
+
+        Dim intMonth As Integer = cbMonth.SelectedIndex
+        lblOut.Text = strDays(intMonth) & " days in " & strMonth(intMonth)
+    End Sub
+End Class
